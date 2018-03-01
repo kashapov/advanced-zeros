83b59b1c32f4350faeb81bbd0baf102f34f5ecdc
Andrey Kashapov <kashapovandrey@gmail.com>
2018-03-01 19:01:14 +0300

    advanced zero v1.3

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  
+  var numberFloor = base;
+  var pow = 0;
+  var con = 0;
+  var k = 0;
+
+  var zeroCnt = number;
+
+  for (var i = 2; i <= base; i++) {
+    if (numberFloor % i == 0) {
+      pow = 0;
+
+      while (numberFloor % i == 0) {
+        pow++;
+        numberFloor = Math.floor(numberFloor/i)
+      }
+
+      con = 0;
+      k = number;
+
+      while (k/i > 0) {
+        con += Math.floor(k/i);
+        k = Math.floor(k/i);
+      }
+
+      zeroCnt = Math.min(zeroCnt, Math.floor(con/pow));
+    }
+  }
+
+  return zeroCnt;
 }
